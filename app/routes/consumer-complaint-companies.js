704920054893c0c53d27ca7e8edeb24b4dc9239b
Charlie Cooksey <ccooksey@bbasolutions.com>
2017-02-18 00:49:18 -0700

    show complaints by company refactor this soon to remove browser URL route and use single route

--- app/routes/consumer-complaint-companies.js
+++ app/routes/consumer-complaint-companies.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model(params) {
-        return this.get('store').findRecord('consumer-complaint-company', 'a74f29cc-9d15-4217-930e-ba034e61056f')
+        return this.get('store').findRecord('consumer-complaint-company', params.id)
     }
 });
